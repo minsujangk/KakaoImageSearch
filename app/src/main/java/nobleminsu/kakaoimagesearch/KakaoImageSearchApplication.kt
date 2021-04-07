85b96faa2155d68f9210014def400d99255bea4a
Minsu Jang <nobleminsu@gmail.com>
2021-04-08 01:17:01 +0900

    Application 클래스 추가

---
+++ app/src/main/java/nobleminsu/kakaoimagesearch/KakaoImageSearchApplication.kt
@@ -0,0 +1,8 @@
+package nobleminsu.kakaoimagesearch
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class KakaoImageSearchApplication : Application() {
+}
