aeca3c95abb64e77c64439675c2902b8f0deb45b
Minsu Jang <nobleminsu@gmail.com>
2021-04-13 22:33:16 +0900

    이미지 디스크 캐시

--- app/src/main/java/nobleminsu/kakaoimagesearch/ui/util/BindingAdapter.kt
+++ app/src/main/java/nobleminsu/kakaoimagesearch/ui/util/BindingAdapter.kt
@@ -3,6 +3,7 @@ package nobleminsu.kakaoimagesearch.ui.util
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import nobleminsu.kakaoimagesearch.R
 
 @BindingAdapter("app:imageUrl")
@@ -10,8 +11,8 @@ fun setImageUrl(imageView: ImageView, imageUrl: String?) {
 //    Timber.e("loading $imageUrl")
     if (imageUrl != null) {
         Glide.with(imageView)
-            .asBitmap()
             .load(imageUrl)
+            .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
             .placeholder(R.drawable.ic_launcher_foreground)
             .into(imageView)
     }
